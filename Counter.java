0b27a7fe6f62b4052ceec73e95860164ae4afcde
Olga <rodakolga@mail.ru>
2024-11-24 15:54:54 +0700

    Finish

---
+++ Counter.java
@@ -0,0 +1,22 @@
+
+
+public class Counter implements AutoCloseable {
+    private int count = 0;
+    private boolean isClosed = false;
+
+    public void add() {
+        if (isClosed) {
+            throw new IllegalStateException("Счетчик закрыт. Нельзя добавлять.");
+        }
+        count++;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    @Override
+    public void close() {
+        isClosed = true;
+    }
+}
